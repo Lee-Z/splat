e098a4af36fb2a76c4712a91ebf8173814ba6b39
lizhan <iali123@163.com>
2022-03-09 19:28:20 +0800

    在任务计划中添加定位查询sql 位置

--- web/crontab.py
+++ web/crontab.py
@@ -2,17 +2,31 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from django_apscheduler.jobstores import DjangoJobStore, register_events, register_job
 import requests
 from web import models
+import bisect
 
 #定时请求客户端状态接口
 scheduler = BackgroundScheduler()  # 创建一个调度器对象
 scheduler.add_jobstore(DjangoJobStore(), "default")  # 添加一个作业
+d = []
+g = int()
+
 try:
+
     # @register_job(scheduler, "interval", seconds=1)用interval方式 每1秒执行一次
 #    @register_job(scheduler, 'cron', day_of_week='mon-sun', hour='8', minute='30', second='10', id='delete_stale_data')  # 定时执行：这里定时为周一到周日每天早上8：30执行一次
-    @register_job(scheduler, "interval", minutes=1, id="time_task", replace_existing=True, misfire_grace_time=120)
+    @register_job(scheduler, "interval", seconds=5, id="time_task", replace_existing=True, misfire_grace_time=120)
     def time_task():
         """定时的任务逻辑"""
-
+        global g
+        print(g)
+        f = models.outgonging_detection.objects.filter(outgong_network=1,outgong_id__gt=g).values('outgong_addr','outgong_id')
+        print("++++++++++++++++++++++")
+        for i in f:
+            print(i['outgong_addr'])
+            g = i['outgong_id']
+            print(g)
+            bisect.insort(d,('北京',i['outgong_addr']))
+        print(d)
         #res = <QuerySet [{'ip': '172.30.2.1'}, {'ip': '172.30.11.100'}, {'ip': '127.0.0.1'}]>
         res = models.Active_ip.objects.values("ip")
         print(res)
